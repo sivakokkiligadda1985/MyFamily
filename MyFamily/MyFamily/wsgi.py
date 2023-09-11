98068536a6cf21b62840bbecb4df9575346463d4
sivakokkiligadda1985 <108756443+sivakokkiligadda1985@users.noreply.github.com>
2023-09-11 15:02:39 +0530

    Update wsgi.py

--- MyFamily/MyFamily/wsgi.py
+++ MyFamily/MyFamily/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/pavani/MyFamily/MyFamily/'
+
+if path not in sys.path:
+
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyFamily.settings')
 
 application = get_wsgi_application()
+
+
+
+
